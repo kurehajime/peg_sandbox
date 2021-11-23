e232dc6e8c927f311a0f40a306112367b2d8b25e
kurehajime <xiidec@gmail.com>
2021-11-23 09:43:49 +0000

    prety

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ const peg_text = fs.readFileSync(peg_file, 'utf8')
 const parser = peggy.generate(peg_text)
 const result = parser.parse('{"abc":"あいうえお","xyz":123}')
 
-console.log(JSON.stringify(result))
+console.log(JSON.stringify(result, null, 2))
