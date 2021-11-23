2c7a63a05147a2f4595b4d5c60e78811ca7617d4
kurehajime <xiidec@gmail.com>
2021-11-23 06:13:19 +0000

    add sample

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const peggy = require("peggy");
+const fs = require('fs')
+const peggy = require("peggy")
 
-console.log("Hello World!");
+const peg_file = "./peggy_example/arithmetics.pegjs"
+const peg_text = fs.readFileSync(peg_file, 'utf8')
+const parser = peggy.generate(peg_text)
+const result = parser.parse("2 * (3 + 4)")
+
+console.log(result)
