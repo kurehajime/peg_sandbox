97aca09ba3b21165f4a6465b3d4a4d61c80349fd
kurehajime <xiidec@gmail.com>
2021-11-23 07:29:03 +0000

    json

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const peggy = require("peggy")
 
-const peg_file = "./peggy_example/arithmetics.pegjs"
+const peg_file = "./peg/peg.pegjs"
 const peg_text = fs.readFileSync(peg_file, 'utf8')
 const parser = peggy.generate(peg_text)
-const result = parser.parse("2 * (3 + 4)")
+const result = parser.parse('{"abc":"あいうえお"}')
 
 console.log(result)
