3169aeb995af439c9cd96b1abd178d63e5ecc048
kurehajime <xiidec@gmail.com>
2021-12-05 09:27:40 +0000

    れーべん

--- typoscript.js
+++ typoscript.js
@@ -14,6 +14,8 @@ function main() {
     const env = new Env()
     result = evaluter.evalute(env, parsed)
     console.log(env)
+
+    console.log(lvd("kitten", "sitting"))
 }
 class Function {
     constructor(_name, _body, _args) {
@@ -29,6 +31,15 @@ class Env {
         this.result = null
         this.is_return = false
     }
+}
+function lvd(s1, s2) {
+    if (s1 === "") { return s2.length }
+    if (s2 === "") { return s1.length }
+    if (s1[0] === s2[0]) { return lvd(s1.slice(1), s2.slice(1)) }
+    let add = lvd(s1.slice(0), s2.slice(1))
+    let del = lvd(s1.slice(1), s2.slice(0))
+    let swap = lvd(s1.slice(1), s2.slice(1))
+    return 1 + Math.min(add, del, swap)
 }
 
 class Evaluter {
